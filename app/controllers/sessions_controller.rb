16ea5606dc5667b749462206a44162cad6b70d81
pajamaw <pjpeterww@gmail.com>
2016-08-08 18:06:13 -0400

    Added login to logout change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     new_zype_session = ZypeService.new
-    puts ENV["ZYPE_CLIENT_ID"]
-    puts ENV["ZYPE_CLIENT_SECRET"]
-    puts "#{params['username']}"
-    puts params['password']
+
     session[:token] = new_zype_session.authenticate!(ENV["ZYPE_CLIENT_ID"], ENV["ZYPE_CLIENT_SECRET"], params['username'], params['password'])
     ##puts session[:token]
     redirect_back_or_root
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,11 +3,6 @@ class VideosController < ApplicationController
 
   def index
     zype = ZypeService.new
-
-    #practicing for authetication turn firewall
-    #@videos = zype.create_videos_from_api.select{|vid| vid.subs == true}
-    #binding.pry
-    #actual method
     @videos = zype.create_videos_from_api
   end
 
@@ -15,15 +10,9 @@ class VideosController < ApplicationController
     zype = ZypeService.new
     videos = zype.create_videos_from_api
     @video = videos.detect{ |vid| vid.id == params['id']}
-    puts !logged_in?
-    puts @video
     if @video.subs && !logged_in?
-      puts @video.subs
-      puts !logged_in?
-      ##session[:video_id] = "/videos/#{@video.id}"
       flash[:alert] = "You Must Login or Subscribe to continue"
       redirect_to '/sessions'
-    #binding.pry
     end
   end
 
