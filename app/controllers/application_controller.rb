65940b70c0ed0356b70927db25d469ef7e97e3c7
pajamaw <pjpeterww@gmail.com>
2016-08-21 16:00:14 -0700

    Changed flash, added incorrect login response and flash message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,14 @@ class ApplicationController < ActionController::Base
     !!session[:token]
   end
 
-  def store_return_to
-    session[:return_to] = request.fullpath
-  end
+  private
+    def store_return_to
+      session[:return_to] = request.fullpath
+    end
 
-  def redirect_back_or_root
-    redirect_to(session[:return_to] || '/')
-    session[:return_to] = nil
-  end
+    def redirect_back_or_root
+      redirect_to(session[:return_to] || '/')
+      session[:return_to] = nil
+    end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,18 @@ class SessionsController < ApplicationController
   def create
     new_zype_session = ZypeService.new
     session[:token] = new_zype_session.authenticate!(ENV["ZYPE_CLIENT_ID"], ENV["ZYPE_CLIENT_SECRET"], params['username'], params['password'])
-
-    redirect_back_or_root
+    if session[:token]
+      flash[:notice] = "You have been logged in"
+      redirect_back_or_root
+    else
+      flash[:alert] = "Incorrect Login"
+      render 'new'
+    end
   end
 
   def destroy
     reset_session
-    flash[:alert] = "You have been logged out"
+    flash[:notice] = "You have been logged out"
 
     redirect_to root_path
   end
